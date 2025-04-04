afce14468049a8bc964e3794d4a6633ad6bf26a2
Jolie <devalkjbr48@lakeforest.edu>
2025-04-04 12:53:09 -0500

    new rproj commit

---
+++ 4.4.25.R
@@ -0,0 +1,12 @@
+# New repository for 4.4.25
+
+
+# -------------------------------------------------------------------------
+library(usethis)
+create_github_token()
+
+
+library(gitcreds)
+gitcreds::gitcreds_set()
+
+use_github()
